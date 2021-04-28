e5ce1021dc1c1fae4c8479cea29fe3af17db83fd
y2sang <37392688+y2sang@users.noreply.github.com>
2021-04-28 17:07:10 +0900

    .

--- do-it-example/src/App.js
+++ do-it-example/src/App.js
@@ -1,25 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import React, { Component } from "react";
+import TodaysPlan from "./03/TodaysPlan";
+import PropComponent from "./03/PropsComponent";
+class App extends Component {
+  render() {
+    return (
+      <div className="body">
+        <TodaysPlan />
+        <PropComponent name="두잇 리액트" />
+      </div>
+    );
+  }
 }
 
 export default App;
