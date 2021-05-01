1ad6d4896654fb86562dbe2073f4fcb26c0a64c8
YS.YOON <bozzang@gmail.com>
2021-05-01 11:30:05 +0900

    .

--- do-it-example/src/App.js
+++ do-it-example/src/App.js
@@ -1,13 +1,13 @@
-
-
-import React, { Component } from 'react';
-
+import React, { Component } from "react";
+import TodaysPlan from "./03/TodaysPlan";
+import PropComponent from "./03/PropsComponent";
 class App extends Component {
   render() {
     return (
-      <div>
+      <div className="body">
+        <TodaysPlan />
+        <PropComponent name="두잇 리액트" />
         <img src='https://picsum.photos/200/300' />
-        <div>안녕하세요</div>
       </div>
     );
   }
