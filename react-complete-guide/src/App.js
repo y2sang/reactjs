0d464e7895ce0e165fe280fbc8d624431a8f658d
YS.YOON <bozzang@gmail.com>
2022-07-15 15:51:39 +0900

    .

--- react-complete-guide/src/App.js
+++ react-complete-guide/src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 
 // import ExpenseItem from "./components/ExpenseItem";
 import Expenses from "./components/Expenses/Expenses";
+import NewExpense from "./components/Expenses/NewExpense/NewExpense";
 
 const App = () => {
     const expenses = [
@@ -14,9 +15,16 @@ const App = () => {
     //     React.createElement('h2', {}, "Let's get started!"),
     //     React.createElement(Expenses, {items: expenses})
     // )
+    const addExpenseHandler = (expense) => {
+        console.log("In App.js");
+
+        expenses.push(expense);
+        console.log(expenses);
+    }
     return (
         <div>
             <h2>Let's get started!</h2>
+            <NewExpense onAddExpense={addExpenseHandler}/>
             <Expenses items={expenses}/>
         </div>
     );
--- react-complete-guide/src/components/Expenses/ExpenseItem.js
+++ react-complete-guide/src/components/Expenses/ExpenseItem.js
@@ -1,11 +1,16 @@
+import React from "react";
 import './ExpenseItem.css';
 import ExpenseDate from "./ExpenseDate";
 import Card from "../UI/Card";
 function ExpenseItem(props) {
+    const [title, setTitle] = React.useState(props.title);
 
-    const expenseItem = props.title;
+    const expenseItem = title;
     const expenseAmount = props.amount;
-
+    const clickHandler = () => {
+        setTitle('Updated');
+        console.log("Clicked!!");
+    }
     return (
         <Card className="expense-item">
             <ExpenseDate date={props.date} />
@@ -13,6 +18,7 @@ function ExpenseItem(props) {
                 <h2>{expenseItem}</h2>
                 <div className="expense-item__price">${expenseAmount}</div>
             </div>
+            <button onClick={clickHandler}>Change Title</button>
         </Card>
     );
 }
