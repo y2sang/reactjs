375cbb7835ba08a7eacde84cbd2b3aa7a5a1b160
YS.YOON <bozzang@gmail.com>
2022-07-19 22:07:38 +0900

    styled Button

--- 01-starting-project/src/components/CourseGoals/CourseInput/CourseInput.js
+++ 01-starting-project/src/components/CourseGoals/CourseInput/CourseInput.js
@@ -11,12 +11,17 @@ const FormControl = styled.div`
     font-weight: bold;
     display: block;
     margin-bottom: 0.5rem;
+    color: ${props => {
+      const {invalid} = props;
+      return invalid ? 'red' : 'black';
+    }};
   }
 
   & input {
     display: block;
     width: 100%;
-    border: 1px solid #ccc;
+    border: 1px solid ${props => (props.invalid ? 'red' : '#ccc')};
+    background: ${props => (props.invalid ? '#ffd7d7' : 'transparent')};
     font: inherit;
     line-height: 1.5rem;
     padding: 0 0.25rem;
@@ -26,15 +31,6 @@ const FormControl = styled.div`
     outline: none;
     background: #fad0ec;
     border-color: #8b005d;
-  }
-
-  &.invalid input {
-    background-color: salmon;
-    border-color: #8b005d;
-  }
-
-  &.invalid label {
-    color: red;
   }
 `;
 
@@ -60,7 +56,7 @@ const CourseInput = props => {
 
     return (
         <form onSubmit={formSubmitHandler}>
-            <FormControl className={!isValid && 'invalid'}>
+            <FormControl invalid={!isValid}>
                 <label>Course Goal</label>
                 <input type="text" onChange={goalInputChangeHandler}/>
             </FormControl>
