0da160332b10fb8a4d0f476fb936cd51188722ef
y2sang <bozzang@gmail.com>
2024-06-07 09:25:17 +0900

    Form Test

--- do-it-example-new/src/main.jsx
+++ do-it-example-new/src/main.jsx
@@ -1,47 +1,133 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
 
-// ReactDOM.createRoot(document.getElementById('root')).render(
-//   <React.StrictMode>
-//     <App />
-//   </React.StrictMode>,
-// )
+function ID() {
+    return '_' + Math.random().toString(36).substr(2, 9);
+}
 
-function ExampleForm() {
-    const [userName, setUserName] = React.useState('');
-    const [userPassword, setUserPassword] = React.useState('');
-    const handleSubmit = (event) => {
-        event.preventDefault();
-        console.log(userName, userPassword);
+class Item {
+    constructor(id, name) {
+        this.id = id;
+        this.name = name;
     }
+}
 
-    const handleChange = (event) => {
-        const {name, value} = event.target;
-        console.log(name, value);
-        console.log({[name]: value});
-        switch (event.target.name) {
-            case 'userName':
-                setUserName(event.target.value);
-                break;
-            case 'userPassword':
-                setUserPassword(event.target.value);
-                break;
-        }
-    }
+const initialItems = [
+    new Item(ID(), 'First Item'),
+    new Item(ID(), 'Second Item'),
+    new Item(ID(), 'Third Item'),
+];
+
+function ListItem({item, onEdit, onRemove}) {
+    return (
+        <p>
+            <span>{item.name}</span>
+            <button onClick={() => onEdit(item)}>Edit</button>
+            <button onClick={() => onRemove(item)}>Remove</button>
+        </p>
+    );
+}
+
+function List({items, onRemove, onUpdate}) {
+    const [editingItem, setEditingItem] = React.useState(null);
+
+    const handleEdit = (item) => {
+        setEditingItem(item);
+    };
+
+    const handleCancel = () => {
+        setEditingItem(null);
+    };
 
     return (
-        <form onSubmit={handleSubmit}>
+        <ul>
+            {items.map((item) => (
+                <li key={item.id}>
+                    {item === editingItem ? (
+                        <Form item={item} onSubmit={onUpdate} onCancel={handleCancel}/>
+                    ) : (
+                        <ListItem item={item} onEdit={handleEdit} onRemove={onRemove}/>
+                    )}
+                </li>
+            ))}
+        </ul>
+    );
+}
 
-            <input name="userName" placeholder="Enter name" value={userName}
-                   onChange={handleChange}/>
-            <input name="userPassword" placeholder="Enter user password"
-                   type={"password"} value={userPassword}
-                   onChange={handleChange}/>
-            <button type="submit">Sign In</button>
+function Form({item, onSubmit, onCancel, buttonValue}) {
+    const [inputValue, setInputValue] = React.useState(item.name || '');
+
+    const handleChange = (event) => {
+        event.preventDefault();
+        setInputValue(event.target.value);
+    };
+
+    const handleFormSubmit = (event) => {
+        event.preventDefault();
+        const submittedItem = {
+            id: item ? item.id : ID(),
+            name: inputValue,
+        };
+
+        onSubmit(submittedItem);
+        setInputValue('');
+    };
+
+    const handleCancel = (event) => {
+        event.preventDefault();
+        onCancel();
+    };
+
+    return (
+        <form onSubmit={handleFormSubmit}>
+            <input value={inputValue} onChange={handleChange}/>
+            <button>{buttonValue || 'Save'}</button>
+            {onCancel && (
+                <a href="#" onClick={handleCancel}>
+                    cancel
+                </a>
+            )}
         </form>
     );
 }
 
-ReactDOM.createRoot(document.getElementById('root')).render(<ExampleForm/>);
+function Container() {
+    const [items, setItems] = React.useState([]);
+
+    React.useEffect(() => setItems(initialItems), []);
+
+    const addItem = (item) => {
+        setItems([...items, item]);
+    };
+
+    const updateItem = (updatedItem) => {
+        let updatedItems = items.map((item) => {
+            return item.id === updatedItem.id
+                ? Object.assign({}, item, updatedItem)
+                : item;
+        });
+        return setItems(updatedItems);
+    };
+
+    const removeItem = (removeThisItem) => {
+        const filteredItems = items.filter((item) => item.id !== removeThisItem.id);
+        setItems(filteredItems);
+    };
+
+    return (
+        <React.Fragment>
+            <Form item="" onSubmit={addItem} buttonValue="Add"/>
+            <List items={items} onRemove={removeItem} onUpdate={updateItem}/>
+        </React.Fragment>
+    );
+}
+
+function App() {
+    return (
+        <div>
+            <Container/>
+        </div>
+    );
+}
+
+ReactDOM.createRoot(document.getElementById('root')).render(<App/>);
