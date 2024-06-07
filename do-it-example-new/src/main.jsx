9cbbd926dc65a546400afbf8f7c36d73dd1ca26f
y2sang <bozzang@gmail.com>
2024-06-07 09:18:28 +0900

    .

--- do-it-example-new/src/main.jsx
+++ do-it-example-new/src/main.jsx
@@ -3,8 +3,45 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+// ReactDOM.createRoot(document.getElementById('root')).render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>,
+// )
+
+function ExampleForm() {
+    const [userName, setUserName] = React.useState('');
+    const [userPassword, setUserPassword] = React.useState('');
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        console.log(userName, userPassword);
+    }
+
+    const handleChange = (event) => {
+        const {name, value} = event.target;
+        console.log(name, value);
+        console.log({[name]: value});
+        switch (event.target.name) {
+            case 'userName':
+                setUserName(event.target.value);
+                break;
+            case 'userPassword':
+                setUserPassword(event.target.value);
+                break;
+        }
+    }
+
+    return (
+        <form onSubmit={handleSubmit}>
+
+            <input name="userName" placeholder="Enter name" value={userName}
+                   onChange={handleChange}/>
+            <input name="userPassword" placeholder="Enter user password"
+                   type={"password"} value={userPassword}
+                   onChange={handleChange}/>
+            <button type="submit">Sign In</button>
+        </form>
+    );
+}
+
+ReactDOM.createRoot(document.getElementById('root')).render(<ExampleForm/>);
