26b05594441419a082ad3ee00967080de9fe5428
y2sang <bozzang@gmail.com>
2024-06-11 13:19:22 +0900

    axios test

--- do-it-example-new/src/main.jsx
+++ do-it-example-new/src/main.jsx
@@ -1,133 +1,67 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 
-function ID() {
-    return '_' + Math.random().toString(36).substr(2, 9);
-}
+const okUrl = 'http://localhost:3000/photos?_page=1&_limit=100';
+// const notFoundErrorUrl = 'https://httpstat.us/404';
+// const forbiddenErrorUrl = 'https://httpstat.us/403';
+// const serverErrorUrl = 'https://httpstat.us/500';
 
-class Item {
-    constructor(id, name) {
-        this.id = id;
-        this.name = name;
+function PhotoList() {
+    const [loading, setLoading] = React.useState(false);
+    const [photos, setPhotos] = React.useState([]);
+    const [error, setError] = React.useState(null);
+
+    function toUserError(error) {
+        console.log('Call API to log the raw error. ', error);
+        return 'There was an error loading the photos.';
+    }
+
+    React.useEffect(() => {
+        setLoading(true);
+
+        fetch(okUrl)
+            .then((response) => {
+                if (!response.ok) throw new Error(response.statusText);
+                return response;
+            })
+            .then((res) => {
+                console.log(res);
+                return res;
+            })
+            .then((response) => {
+                return response.json();
+            })
+            .then((data) => {
+                console.log(data);
+                setError(null);
+                setPhotos(data);
+                setLoading(false);
+            })
+            .catch((error) => {
+                const userError = toUserError(error);
+                setError(userError);
+                setLoading(false);
+            });
+    }, []);
+
+    if (error) {
+        return <div>{error}</div>;
+    } else if (loading) {
+        return <div>Loading...</div>;
+    } else {
+        return (
+            <ul>
+                {photos.map((photo) => {
+                    return (
+                        <li key={photo.id}>
+                            <img src={photo.thumbnailUrl} alt={photo.title} />
+                            <h3>{photo.title}</h3>
+                        </li>
+                    );
+                })}
+            </ul>
+        );
     }
 }
 
-const initialItems = [
-    new Item(ID(), 'First Item'),
-    new Item(ID(), 'Second Item'),
-    new Item(ID(), 'Third Item'),
-];
-
-function ListItem({item, onEdit, onRemove}) {
-    return (
-        <p>
-            <span>{item.name}</span>
-            <button onClick={() => onEdit(item)}>Edit</button>
-            <button onClick={() => onRemove(item)}>Remove</button>
-        </p>
-    );
-}
-
-function List({items, onRemove, onUpdate}) {
-    const [editingItem, setEditingItem] = React.useState(null);
-
-    const handleEdit = (item) => {
-        setEditingItem(item);
-    };
-
-    const handleCancel = () => {
-        setEditingItem(null);
-    };
-
-    return (
-        <ul>
-            {items.map((item) => (
-                <li key={item.id}>
-                    {item === editingItem ? (
-                        <Form item={item} onSubmit={onUpdate} onCancel={handleCancel}/>
-                    ) : (
-                        <ListItem item={item} onEdit={handleEdit} onRemove={onRemove}/>
-                    )}
-                </li>
-            ))}
-        </ul>
-    );
-}
-
-function Form({item, onSubmit, onCancel, buttonValue}) {
-    const [inputValue, setInputValue] = React.useState(item.name || '');
-
-    const handleChange = (event) => {
-        event.preventDefault();
-        setInputValue(event.target.value);
-    };
-
-    const handleFormSubmit = (event) => {
-        event.preventDefault();
-        const submittedItem = {
-            id: item ? item.id : ID(),
-            name: inputValue,
-        };
-
-        onSubmit(submittedItem);
-        setInputValue('');
-    };
-
-    const handleCancel = (event) => {
-        event.preventDefault();
-        onCancel();
-    };
-
-    return (
-        <form onSubmit={handleFormSubmit}>
-            <input value={inputValue} onChange={handleChange}/>
-            <button>{buttonValue || 'Save'}</button>
-            {onCancel && (
-                <a href="#" onClick={handleCancel}>
-                    cancel
-                </a>
-            )}
-        </form>
-    );
-}
-
-function Container() {
-    const [items, setItems] = React.useState([]);
-
-    React.useEffect(() => setItems(initialItems), []);
-
-    const addItem = (item) => {
-        setItems([...items, item]);
-    };
-
-    const updateItem = (updatedItem) => {
-        let updatedItems = items.map((item) => {
-            return item.id === updatedItem.id
-                ? Object.assign({}, item, updatedItem)
-                : item;
-        });
-        return setItems(updatedItems);
-    };
-
-    const removeItem = (removeThisItem) => {
-        const filteredItems = items.filter((item) => item.id !== removeThisItem.id);
-        setItems(filteredItems);
-    };
-
-    return (
-        <React.Fragment>
-            <Form item="" onSubmit={addItem} buttonValue="Add"/>
-            <List items={items} onRemove={removeItem} onUpdate={updateItem}/>
-        </React.Fragment>
-    );
-}
-
-function App() {
-    return (
-        <div>
-            <Container/>
-        </div>
-    );
-}
-
-ReactDOM.createRoot(document.getElementById('root')).render(<App/>);
+ReactDOM.createRoot(document.getElementById('root')).render(<PhotoList />);
